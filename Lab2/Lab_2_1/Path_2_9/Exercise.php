aec52bf322f33a0a15d1218e9062c717b02db7fe
nhattuan1606 <tuanha742@gmail.com>
2022-04-28 10:50:21 +0700

    update 2 lab 2.1

--- Lab2/Lab_2_1/Path_2_9/Exercise.php
+++ Lab2/Lab_2_1/Path_2_9/Exercise.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="vi">
 <head>
     <title> Exercise </title>
 </head>
@@ -9,35 +9,19 @@
         $class = $_POST["class"];
         $university = $_POST["university"];
 
-        $hobbies = array();
-        $tmp = $_POST["cat"];
-        if ($tmp == "Yes") $hobbies[] = "Cat";
-
-        $tmp = $_POST["cooking"];
-        if ($tmp == "Yes") $hobbies[] = "Cooking";
-
-        $tmp = $_POST["football"];
-        if ($tmp == "Yes") $hobbies[] = "Football";
-
-        $tmp = $_POST["math"];
-        if ($tmp == "Yes") $hobbies[] = "Math";
-
-        $tmp = $_POST["web"];
-        if ($tmp == "Yes") $hobbies[] = "Web";
-
-        $tmp = $_POST["facebook"];
-        if ($tmp == "Yes") $hobbies[] = "Facebook";
-
-        $tmp = $_POST["youtube"];
-        if ($tmp == "Yes") $hobbies[] = "Youtube";
-
         print "Hello, $name";
         print "<br>You are studying at $class, $university";
         print "<br>Your hobby is";
-        foreach ($hobbies as $key=>$value) {
-            $i = $key + 1;
-            print "<br>$i. $value";
-        }
     ?>
+    <ol>
+        <?php
+            if (array_key_exists("hobby", $_POST)) {
+                $hobby = $_POST["hobby"];
+                foreach ($hobby as $i) {
+                    print "<li>$i</li>";
+                }
+            }
+        ?>
+    </ol>
 </body>
 </html>
