b3d8abb01d03923c8c389937a535bc6cfcdfa323
DESKTOP-UNVBULM\Admin <xuantungkhuong@gmail.com>
2022-07-07 22:33:09 +0800

    Thêm hàm findByCriteria_base vào BaseModel

--- library_be/models/BaseModel.php
+++ library_be/models/BaseModel.php
@@ -31,6 +31,13 @@ class BaseModel extends database {
         return mysqli_fetch_assoc($query);
     }
 
+    public function findByCriteria_base($table, $column_name, $column, $select = ['*']) {
+        $columns = implode(',', $select);
+        $sql = "select ${columns} from ${table} where ${column_name} = ${column}";
+        $query = $this->_query($sql);
+        return mysqli_fetch_assoc($query);
+    }
+
     public function create_base($table, $data): string
     {
         $columns = implode(',', array_keys($data));
